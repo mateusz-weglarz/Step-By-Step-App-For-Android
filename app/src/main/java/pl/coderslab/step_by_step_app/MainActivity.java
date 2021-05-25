a307f912e2b903b01432922fd4f178d26ee16a9b
mateusz-weglarz <mjw1992@interia.pl>
2021-05-25 20:34:20 +0200

    Pause + Permission button, zgoda na dostęp aplikacji do ACTIVITY_RECOGNITION.

--- app/src/main/java/pl/coderslab/step_by_step_app/MainActivity.java
+++ app/src/main/java/pl/coderslab/step_by_step_app/MainActivity.java
@@ -1,6 +1,10 @@
 package pl.coderslab.step_by_step_app;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,8 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -23,8 +30,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private TextView count_steps;
     private Button startButton;
+    private Button pauseButton;
     private Button endButton;
+    private Button permissionButton;
     private float stepCount;
+    private boolean isSensorActive = false;
+    private final int ACTIVITY_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,28 +44,88 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         count_steps = findViewById(R.id.count_steps);
         startButton = findViewById(R.id.startButton);
+        pauseButton = findViewById(R.id.pauseButton);
         endButton = findViewById(R.id.endButton);
+        permissionButton = findViewById(R.id.permissionButton);
         startButton.setOnClickListener(v -> {
             Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             if (countSensor != null) {
                 startButton.setEnabled(false);
+                pauseButton.setEnabled(true);
                 endButton.setEnabled(true);
                 sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_FASTEST);
+                isSensorActive = true;
             } else {
                 Toast.makeText(this, "Nie znaleziono sensora.", Toast.LENGTH_SHORT).show();
             }
         });
+        pauseButton.setOnClickListener(v -> {
+            startButton.setEnabled(true);
+            pauseButton.setEnabled(false);
+            sensorManager.unregisterListener(this);
+            isSensorActive = false;
+        });
         endButton.setOnClickListener(v -> {
             startButton.setEnabled(true);
+            pauseButton.setEnabled(false);
             endButton.setEnabled(false);
-            sensorManager.unregisterListener(this);
+            if (isSensorActive) {
+                sensorManager.unregisterListener(this);
+            }
             try {
                 sendRequest();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         });
+        permissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "You have already granted this permission.", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestActivityRecognitionPermission();
+                }
+            }
+        });
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    private void requestActivityRecognitionPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed for application to run.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, ACTIVITY_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, ACTIVITY_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == ACTIVITY_PERMISSION_CODE){
+            if(grantResults.length>0&& grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission GRANTED",Toast.LENGTH_SHORT).show();
+                permissionButton.setEnabled(false);
+            }else{
+                Toast.makeText(this,"Permission DENIED",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -70,7 +141,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private void sendRequest() throws JSONException {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.PUT, "https://mateuszweglarz.free.beeceptor.com/test", new JSONObject().put("stepCount",stepCount), new Response.Listener<JSONObject>() {
+                (Request.Method.PUT, "https://mateuszweglarz.free.beeceptor.com/test", new JSONObject().put("stepCount", stepCount), new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
@@ -80,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
